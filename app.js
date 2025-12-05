54c5fdf17af994536c8cd15b5aacc88a71d0f4e7
Dylan <dylan.smith3107@gmail.com>
2025-12-05 10:23:16 +0000

    feat: completed required functionality of purchase button via a conditional, also added more user clarity to the shop display

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ let stats = {
 const stringifiedStats = JSON.stringify(stats);
 localStorage.setItem("stats", stringifiedStats);
 
+const cookieStats = document.getElementById("cookie-stats");
+
+const cookieCountText = document.createElement("h2");
+cookieCountText.textContent = `Cookie count: ${stats.totalCookieCount}`;
+cookieStats.appendChild(cookieCountText);
+
+const cpsText = document.createElement("h3");
+cpsText.textContent = `Cookies per second (cps): ${stats.cps}`;
+cookieStats.appendChild(cpsText);
+
 //=======================================================
 //Shop upgrades
 
@@ -26,23 +36,28 @@ const shopContainer = document.getElementById("shop-container");
 function createShop(shopData) {
   for (let i = 0; i < shopData.length; i++) {
     const div = document.createElement("div");
-    div.className = "shopDiv";
+    div.className = "shop-div";
     shopContainer.appendChild(div);
     const upgradeName = document.createElement("h3");
     upgradeName.textContent = shopData[i].name;
     div.appendChild(upgradeName);
     const upgradeCost = document.createElement("p");
-    upgradeCost.textContent = shopData[i].cost;
+    upgradeCost.textContent = `Cost: ${shopData[i].cost}`;
     div.appendChild(upgradeCost);
     const cpsIncrease = document.createElement("p");
-    cpsIncrease.textContent = shopData[i].increase;
+    cpsIncrease.textContent = `Increase: ${shopData[i].increase} CPS`;
     div.appendChild(cpsIncrease);
     const purchaseBtn = document.createElement("button");
     purchaseBtn.textContent = "Purchase";
     purchaseBtn.className = "purchase-btn";
     div.appendChild(purchaseBtn);
+    const notEnoughCookies = document.createElement("p");
+    notEnoughCookies.textContent =
+      "You do not have enough cookies to purchase this upgrade";
+    notEnoughCookies.className = "not-enough-cookies";
+    div.appendChild(notEnoughCookies);
     purchaseBtn.addEventListener("click", function () {
-      purchaseUpgrade(i, shopData);
+      purchaseUpgrade(i, shopData, notEnoughCookies);
     });
   }
 }
@@ -52,10 +67,17 @@ async function renderShop() {
   createShop(shopApi);
 }
 
-function purchaseUpgrade(iValue, shopData) {
-  stats.totalCookieCount = stats.totalCookieCount - shopData[iValue].cost;
-  stats.cps = stats.cps + shopData[iValue].increase;
-  localStorage.setItem("stats", stringifiedStats);
+function purchaseUpgrade(iValue, shopData, notEnoughCookies) {
+  if (stats.totalCookieCount >= shopData[iValue].cost) {
+    stats.totalCookieCount = stats.totalCookieCount - shopData[iValue].cost;
+    stats.cps = stats.cps + shopData[iValue].increase;
+    localStorage.setItem("stats", stringifiedStats);
+  } else {
+    notEnoughCookies.style.display = "flex";
+    setTimeout(function () {
+      notEnoughCookies.style.display = "none";
+    }, 3000);
+  }
 }
 
 renderShop();
